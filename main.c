d2921c6d3e81499d257f7b98ee4a5da600496985
Curseim <49472048+Curseim@users.noreply.github.com>
2021-07-28 18:07:46 +0900

    Update main.c

--- main.c
+++ main.c
@@ -7,84 +7,168 @@
 #include <process.h>
 #include <windows.h>
 
-void Manager_Sort(int *arr, int *arr_cpy, int *timeTaken_Sort_Arr);
+int Manager_Input();
+void Manager_SortSelector(int *stat);
+void Manager_Sort(int *arr, int *arr_cpy, int *timeTaken_Sort_Arr, int *stat);
 void Sort_Bubble(int *arr);
 void Sort_Selection(int *arr);
 void Sort_Insertion(int *arr);
 void Sort_Merge(int *arr, int left, int right);
 void Merge(int *arr, int left, int mid, int right);
-void Sort_Quick(int *arr);
+void Sort_Quick(int *arr, int left, int right);
+int partition(int *arr, int left, int right);
 void Swap(int *a,int *b);
 int Calc_Time(int start, int end);
 int ArrCount(int *arr);
 void ArrCpy(int *arr, int *arr_cpy);
-void PrintDisplay(int *arr, int *timeTaken_Sort_Arr);
+void PrintDisplay(int *arr, int *timeTaken_Sort_Arr, int *stat);
 
 void main() 
 {
-	srand(time(NULL));
+	srand(time(NULL));	
 	
-	int count;
-	int timeTaken_Sort_Arr[5];
+	int count, stat[5] = { 1, 1, 1, 1, 1 };
+	int timeTaken_Sort_Arr[5];	
 	
-	printf("생성 할 수의 개수 입력 > ");
-	scanf("%d", &count);
-	system("cls");
-	
-	int *arr = (int *)malloc(sizeof(int) * count);
-	int *arr_cpy = (int *)malloc(sizeof(int) * count);
-		
-	for (int i = 0; i < count; i++)
+	while (1) 
 	{
-		arr[i] = rand() % 9999 + 1;
-	}
-
-	Manager_Sort(arr, arr_cpy, timeTaken_Sort_Arr);
-	PrintDisplay(arr_cpy, timeTaken_Sort_Arr);
+		while (count == 0) 
+		{
+			count = Manager_Input();
+			
+			if (count == 0) 
+			{
+				Manager_SortSelector(stat);
+			}			
+		}
+		
+		int *arr = (int *)malloc(sizeof(int) * count);
+		int *arr_cpy = (int *)malloc(sizeof(int) * count);
+			
+		for (int i = 0; i < count; i++)
+		{
+			arr[i] = rand() % 1000 + 1;
+		}
+	
+		Manager_Sort(arr, arr_cpy, timeTaken_Sort_Arr, stat);
+		PrintDisplay(arr_cpy, timeTaken_Sort_Arr, stat);		
+		
+		count = 0;
+		free(arr);
+		free(arr_cpy);
+	}				
 }
-void Manager_Sort(int *arr, int *arr_cpy, int *timeTaken_Sort_Arr) 
+int Manager_Input() 
+{
+	int count;
+	
+	printf("\n\n   [ 0 ] 활성/비활성 정렬 선택\n\n");
+	printf("   생성 할 수의 개수 입력 > ");
+	scanf("%d", &count);
+	system("cls");	
+	
+	return count;
+}
+void Manager_SortSelector(int *stat) 
+{
+	int select;
+	
+	printf("\n\n   상태\n\n");
+	
+	for (int i = 0; i < 5; i++) 
+	{
+		if (stat[i] == 1) 
+		{
+			printf("    O    ");
+		} 
+		else if (stat[i] == 0)
+		{
+			printf("    X    ");
+		}
+		
+		switch (i) 
+		{
+			case 0 :
+				printf("버블 [ 1 ]\n");
+				break;
+			case 1 :
+				printf("선택 [ 2 ]\n");	
+				break;	
+			case 2 :
+				printf("삽입 [ 3 ]\n");	
+				break;	
+			case 3 :
+				printf("병합 [ 4 ]\n");	
+				break;	
+			case 4 :
+				printf("  퀵 [ 5 ]\n");	
+				break;					
+		}		
+	}
+	printf("\n         뒤로 [ 0 ]\n");
+	printf("\n              > ");
+	scanf("%d", &select);
+	
+	switch (select) 
+	{						
+		case 1 :
+			stat[0] = !stat[0];
+			break;
+		case 2 :
+			stat[1] = !stat[1];
+			break;				
+		case 3 :
+			stat[2] = !stat[2];
+			break;				
+		case 4 :
+			stat[3] = !stat[3];
+			break;				
+		case 5 :
+			stat[4] = !stat[4];
+			break;											
+	}
+	
+	system("cls");	
+}
+void Manager_Sort(int *arr, int *arr_cpy, int *timeTaken_Sort_Arr, int *stat) 
 {
 	int start, end;
 	
-/*	ArrCpy(arr, arr_cpy);
-	start = clock();
-	printf("\n\n버블 정렬 계산 중...\n\n");
-	Sort_Bubble(arr_cpy);
-	end = clock();
-	timeTaken_Sort_Arr[0] = Calc_Time(start, end);
-	system("cls");
-	
-	ArrCpy(arr, arr_cpy);
-	start = clock();
-	printf("\n\n선택 정렬 계산 중...\n\n");
-	Sort_Selection(arr_cpy);
-	end = clock();
-	timeTaken_Sort_Arr[1] = Calc_Time(start, end);
-	system("cls");
-	
-	ArrCpy(arr, arr_cpy);	
-	start = clock();
-	printf("\n\n삽입 정렬 계산 중...\n\n");
-	Sort_Insertion(arr_cpy);
-	end = clock();
-	timeTaken_Sort_Arr[2] = Calc_Time(start, end);
-	system("cls");														*/
-	
-	ArrCpy(arr, arr_cpy);
-	start = clock();
-	printf("\n\n병합 정렬 계산 중...\n\n");
-	Sort_Merge(arr_cpy, 0, ArrCount(arr) - 1);
-	end = clock();
-	timeTaken_Sort_Arr[3] = Calc_Time(start, end);
-	system("cls");
-	
-	ArrCpy(arr, arr_cpy);
-	start = clock();
-	printf("\n\n  퀵 정렬 계산 중...\n\n");
-	Sort_Quick(arr_cpy);
-	end = clock();
-	timeTaken_Sort_Arr[4] = Calc_Time(start, end);
-	system("cls");
+	for (int i = 0; i < 5; i++) 
+	{			
+		if (stat[i] == 1)
+		{
+			ArrCpy(arr, arr_cpy);
+			start = clock();			
+					
+			switch (i)
+			{
+				case 0 :
+					printf("\n\n버블 정렬 계산 중...\n\n");	
+					Sort_Bubble(arr_cpy);
+					break;
+				case 1 :
+					printf("\n\n선택 정렬 계산 중...\n\n");
+					Sort_Selection(arr_cpy);
+					break;
+				case 2 :
+					printf("\n\n삽입 정렬 계산 중...\n\n");
+					Sort_Insertion(arr_cpy);
+					break;
+				case 3 :
+					printf("\n\n병합 정렬 계산 중...\n\n");
+					Sort_Merge(arr_cpy, 0, ArrCount(arr) - 1);
+					break;
+				case 4 :
+					printf("\n\n  퀵 정렬 계산 중...\n\n");
+					Sort_Quick(arr_cpy, 0, ArrCount(arr) - 1);
+					break;				
+			}
+			end = clock();
+			timeTaken_Sort_Arr[i] = Calc_Time(start, end);		
+			system("cls");			
+		}	
+	}			
 }
 int Calc_Time(int start, int end)
 {
@@ -197,11 +281,33 @@ void Merge(int *arr, int left, int mid, int right)
 	
 	free(sorted); 
 }
-void Sort_Quick(int *arr) 
+void Sort_Quick(int *arr, int left, int right)
 {
-	
+  if (left < right)
+  {
+    int p = partition(arr, left, right);
+
+    Sort_Quick(arr, left, p - 1);
+    Sort_Quick(arr, p + 1, right);
+  }
 }
-void Swap(int *a, int *b) 
+int partition(int *arr, int left, int right)
+{
+	int pivot = arr[right];
+	int i = (left - 1);
+	
+	for (int j = left; j <= (right - 1); j++) 
+	{			
+		if (arr[j] <= pivot) 
+		{
+			i++;
+			Swap(&arr[i], &arr[j]);
+		}
+	}
+	Swap(&arr[i + 1], &arr[right]);
+	return (i + 1);
+}
+void Swap(int *a, int *b)
 {
 	int temp = *a;
 	*a = *b;
@@ -218,7 +324,7 @@ void ArrCpy(int *arr, int *arr_cpy)
 		arr_cpy[i] = arr[i];
 	}
 }
-void PrintDisplay(int *arr, int *timeTaken_Sort_Arr)
+void PrintDisplay(int *arr, int *timeTaken_Sort_Arr, int *stat)
 {
 	int line = 0;
 	
@@ -264,7 +370,14 @@ void PrintDisplay(int *arr, int *timeTaken_Sort_Arr)
 				printf("     퀵 정렬");
 				break;
 		}
-		printf(" : %d ms\n", timeTaken_Sort_Arr[i]);
+		if (stat[i] == 1) 
+		{
+			printf(" : %5d ms\n", timeTaken_Sort_Arr[i]);			
+		}
+		else 
+		{
+			printf(" :       ms\n");		
+		}
 	}
 
 }
